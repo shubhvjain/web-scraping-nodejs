6b7da71b063dffe4c2571fecac50339b4689b477
Shubh V Jain <shubhvjain@gmail.com>
2021-01-06 22:21:33 +0530

    working on http server

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-// Create http server 
-// access control - same origin 
+const logger = require('morgan');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('port', port);
+app.use(logger('dev'));
+app.listen(port, () => console.log(`App started on port ${port}.`));
