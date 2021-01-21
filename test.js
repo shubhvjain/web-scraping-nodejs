01b0f7e3b47907b02b93a17218bf56304e713fa1
Shubh V Jain <shubhvjain@gmail.com>
2021-01-21 18:36:20 +0530

    getting youtube video metadata

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 const book = require("./book")
 const website = require("./website")
+const video = require("./videos")
 // book.getBookDetails(["9780349121086","9788120305960"]).then(data => {
 //     console.log(JSON.stringify(data,null,2))
 // }).catch(err=>{console.log(err)})
 
-website.getMetaData("https://css-tricks.com/essential-meta-tags-social-media/").then(data => {
-    console.log(JSON.stringify(data,null,2))
-//     console.log(JSON.stringify(data,null,2))
- }).catch(err=>{console.log(err)})
+//website.getMetaData("https://css-tricks.com/essential-meta-tags-social-media/").then(data => {
+//    console.log(JSON.stringify(data,null,2))
+//}).catch(err=>{console.log(err)})
+
+video.getYoutubeMetaData(["i0CkaQ9mWAw"])
+    .then(data => {
+        console.log(JSON.stringify(data, null, 2))
+    })
