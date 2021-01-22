af68378d63163b183cf21f3eecf4f4b418895c72
Shubh V Jain <shubhvjain@gmail.com>
2021-01-22 16:52:18 +0530

    scrape api , body-parser

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const logger = require('morgan');
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   if (req.method === 'OPTIONS') {
     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET")
-    return res.status(200).json({});
+    return res.status(200).json({})
   }
   next();
 })
@@ -22,23 +25,31 @@ let scrape = async(type,data) =>{
       'website':()=>{},
       'youtube':()=>{}
     }
-    if(!types[type]){throw new Error("Item not found")}
-    
+    if(!types[type]){throw new Error("Item type to scrape not found")}
+    return {}
   } catch (error) {
    throw error 
   }
 }
 
-app.post('/', (req, res)=> {
+app.post('/', async (req, res)=> {
   try {
-    
+    if(!req.body.type){throw new Error("Item not provided")}
+    if(!req.body.data){throw new Error("Data not provided")}
+
+    let type = req.body.type
+    let data =  req.body.data
+    let scrapedData = await scrape(type,data)
+    res.json({type:type,data:scrapedData})
+
   } catch (error) {
-    res.json({ "error": error.message }); 
+    console.log("Error: "+error.message)
+    res.json({ "error": error.message })
   }
 });
 
-const port = process.env.PORT || 3220;
+const port = process.env.PORT || 3220
 
-app.set('port', port);
-app.use(logger('dev'));
-app.listen(port, () => console.log(`App started on port ${port}.`));
+app.set('port', port)
+app.use(logger('dev'))
+app.listen(port, () => console.log(`App started on port ${port}.`))
